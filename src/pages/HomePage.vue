808f033e683a184e0b0e96897024b926618119ee
pranav2050 <97501797+pranavpeter2050@users.noreply.github.com>
2023-04-02 10:53:29 +0530

    "min-height" for post-image.
    Created card element to show Insta-post.

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,6 +1,25 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page class="constrain q-pa-md">
+    <q-card class="card-post" flat bordered>
+      <q-item>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+          </q-avatar>
+        </q-item-section>
 
+        <q-item-section>
+          <q-item-label>ryan_reynolds</q-item-label>
+          <q-item-label caption>
+            Vancity, Canada
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-separator />
+
+      <q-img src="https://cdn.quasar.dev/img/mountains.jpg"></q-img>
+    </q-card>
   </q-page>
 </template>
 
@@ -11,3 +30,9 @@ export default defineComponent({
   name: 'HomePage'
 })
 </script>
+
+<style lang="sass">
+.card-post
+  .q-img
+    min-height: 200px
+</style>

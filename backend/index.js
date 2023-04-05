94cd2ab7a9bc7628c6329b50abf52c9ad351d0f5
pranav2050 <97501797+pranavpeter2050@users.noreply.github.com>
2023-04-06 02:40:08 +0530

    Uploading image to Firebase Storage and storing resulting "imageUrl" in Firebase DB.
    - npm install uuid-v4 --save (https://youtu.be/Tnz2K_tX_qo?t=2225)
    -

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,12 @@
 const express = require('express')
 const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
 const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+const { getStorage } = require('firebase-admin/storage');
 let busboy = require('busboy');
+let path = require('path');
+let os = require('os');
+let fs = require('fs');
+let UUID = require('uuid-v4');
 
 /* config - express */
 const app = express()
@@ -11,10 +16,12 @@ const app = express()
 const serviceAccount = require('./serviceAccountKey.json');
 
 initializeApp({
-  credential: cert(serviceAccount)
+  credential: cert(serviceAccount),
+  storageBucket: 'community-9b01c.appspot.com'
 });
 
 const db = getFirestore();
+const bucket = getStorage().bucket();
 
 /* api endpoint - posts */
 app.get('/posts', (request, response) => {
@@ -36,10 +43,13 @@ app.get('/posts', (request, response) => {
 app.post('/createPost', (request, response) => {
   response.set('Access-Control-Allow-Origin', '*')
 
+  let uuid = UUID()
+
   console.log('POST request');
   const busboyy = busboy({ headers: request.headers });
 
   let fields = {}
+  let fileData = {}
 
   busboyy.on('file', (name, file, info) => {
     const { filename, encoding, mimeType } = info;
@@ -49,11 +59,10 @@ app.post('/createPost', (request, response) => {
       encoding,
       mimeType
     );
-    file.on('data', (data) => {
-      console.log(`File [${name}] got ${data.length} bytes`);
-    }).on('close', () => {
-      console.log(`File [${name}] done`);
-    });
+
+    let filepath = path.join(os.tmpdir(), filename)
+    file.pipe(fs.createWriteStream(filepath))
+    fileData = { filepath, mimeType }
   });
 
   busboyy.on('field', (name, val, info) => {
@@ -64,19 +73,40 @@ app.post('/createPost', (request, response) => {
   busboyy.on('close', () => {
     // console.log('fields: ', fields)
 
-    // Add a new document in collection "posts"
-    db.collection('posts').doc(fields.id).set({
-      id: fields.id,
-      caption: fields.caption,
-      location: fields.location,
-      // date: fields.date, // if we direct send like this then the date will be saved as a "string" but we need to save it as an "integer"
-      date: parseInt(fields.date),
-      imageUrl: 'https://firebasestorage.googleapis.com/v0/b/community-9b01c.appspot.com/o/Slider_01.jpg?alt=media&token=67399992-8018-4ee8-81fc-6c6060a434bc'
-    });
+    bucket.upload(
+      fileData.filepath,
+      {
+        uploadType: 'media',
+        metadata: {
+          metadata: {
+            contentType: fileData.mimeType,
+            firebaseStorageDownloadTokens: uuid
+          }
+        }
+      },
+      (error, uploadedFile) => {
+        if (!error) {
+          createDocument(uploadedFile)
+        }
+      }
+    )
 
+    function createDocument(uploadedFile) {
+
+      // Add a new document in collection "posts"
+      db.collection('posts').doc(fields.id).set({
+        id: fields.id,
+        caption: fields.caption,
+        location: fields.location,
+        date: parseInt(fields.date),
+        imageUrl: `https://firebasestorage.googleapis.com/v0/b/${ bucket.name }/o/${ uploadedFile.name }?alt=media&token=${ uuid }`
+      }).then(() => {
+        response.send("post id: " + fields.id)
+      })
+    }
     // console.log('Done parsing form!');
     // response.end(); // response.data object will be empty, so instead use below line
-    response.send("Done parsing form!")
+    // response.send("Done parsing form!")
   });
 
   request.pipe(busboyy);
